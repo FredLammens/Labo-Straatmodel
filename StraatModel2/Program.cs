6128d659caea96e75af007e7f2189f195c738448
Frederic Lammens <frederic.lammens@student.hogent.be>
2020-04-11 00:32:38 +0200

    Tool 3 straatIDs finished

--- StraatModel2/Program.cs
+++ StraatModel2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using StraatModel2;
 using StraatModel2.Tool2;
+using StraatModel2.Tool3;
 using System.Collections.Generic;
 
 namespace Labo
@@ -8,9 +9,14 @@ namespace Labo
     {
         static void Main(string[] args)
         {
-            DatabaseImporter db = new DatabaseImporter(@"Data Source=DESKTOP-OF28PIK\SQLEXPRESS;Initial Catalog=provincies; Integrated Security=True");
-            db.InsertAll(Factories.ProvincieFactory());
-            System.Console.WriteLine("done");
+            //DatabaseImporter db = new DatabaseImporter(@"Data Source=DESKTOP-OF28PIK\SQLEXPRESS;Initial Catalog=provincies; Integrated Security=True");
+            //db.InsertAll(Factories.ProvincieFactory());
+            //System.Console.WriteLine("done");
+            DatabaseBevragingen dbv = new DatabaseBevragingen(@"Data Source=DESKTOP-OF28PIK\SQLEXPRESS;Initial Catalog=provincies; Integrated Security=True");
+            foreach (var id in dbv.straatIDs("Gent"))
+            {
+                System.Console.WriteLine(id);
+            }  
         }
     }
 }
---
+++ StraatModel2/Tool3/DatabaseBevragingen.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace StraatModel2.Tool3
+{
+    class DatabaseBevragingen
+    {
+        private string connectionString;
+        public DatabaseBevragingen(string connectionString) => (this.connectionString) = (connectionString);
+        private SqlConnection GetConnection()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+        /// <summary>
+        /// Geeft lijst van straatIDs voor opgegeven gemeentenaam
+        /// </summary>
+        /// <param name="gemeentenaam"></param>
+        public List<int> straatIDs(string gemeentenaam)
+        {
+            List<int> straten = new List<int>();
+            //gemeenteid verkrijgen via gemeentenaam
+            string gemeenteIdQuery = "SELECT id " +
+                                     "FROM Gemeente " +
+                                     "WHERE naam = @gemeentenaam";
+            //via gemeenteid straatids krijgen
+            string straatIdQuery = "SELECT id " +
+                                   "FROM Straat " +
+                                   "WHERE gemeente = @gemeenteId";
+
+            using (SqlConnection connection = GetConnection())
+            {
+                try
+                {
+                    SqlCommand command1 = new SqlCommand(gemeenteIdQuery, connection);
+                    command1.Parameters.AddWithValue("@gemeentenaam", gemeentenaam);
+                    connection.Open();
+                    int gemeenteId = (int)command1.ExecuteScalar();
+                    SqlCommand command2 = new SqlCommand(straatIdQuery, connection);
+                    command2.Parameters.AddWithValue("@gemeenteId", gemeenteId);
+                    SqlDataReader reader = command2.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        straten.Add((int)reader["id"]);
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                finally 
+                {
+                    connection.Close();
+                }
+            }
+            return straten;
+        }
+    }
+}
