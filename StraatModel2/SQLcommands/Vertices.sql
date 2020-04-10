446d6d1dd9f404f1c998f8689a1a8c3ae2617660
Frederic Lammens <frederic.lammens@student.hogent.be>
2020-04-10 14:35:33 +0200

    DatabaseImporter verder werken

--- StraatModel2/SQLcommands/Vertices.sql
+++ StraatModel2/SQLcommands/Vertices.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[Vertices] (
     [verticeX]  FLOAT (53) NOT NULL,
     [verticeY]  FLOAT (53) NOT NULL,
-    [segmentId] INT        NOT NULL,
+    [segment] INT        NOT NULL,
     CONSTRAINT [FK_Vertices_Punt] FOREIGN KEY ([verticeX], [verticeY]) REFERENCES [dbo].[Punt] ([x], [y]),
-    CONSTRAINT [FK_Vertices_Segment] FOREIGN KEY ([segmentId]) REFERENCES [dbo].[Segment] ([id])
+    CONSTRAINT [FK_Vertices_Segment] FOREIGN KEY ([segment]) REFERENCES [dbo].[Segment] ([id])
 );
 
--- StraatModel2/Tool2/DatabaseImporter.cs
+++ StraatModel2/Tool2/DatabaseImporter.cs
@@ -1,6 +1,8 @@
 ﻿using Labo;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace StraatModel2.Tool2
@@ -9,19 +11,60 @@ namespace StraatModel2.Tool2
     {
         private readonly string connectionString;
         public DatabaseImporter(string connectionString) => (this.connectionString) = (connectionString);
-        public void Insert(List<Provincie> provincies) 
+        public void InsertAll(List<Provincie> provincies)
         {
-            //waarden die in databanken moeten
-            List<Punt> punten = new List<Punt>();
-            List<Knoop> knopen = new List<Knoop>();
-            List<Segment> segmenten = new List<Segment>();
-            List<Graaf> graven = new List<Graaf>();
-            List<Straat> straten = new List<Straat>();
-            List<Gemeente> gemeentes = new List<Gemeente>();
-
-
-
-
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+            }
+        }
+        private void GetDataTables(List<Provincie> provincies)
+        {
+            //provincie
+            DataTable provinciesDT = new DataTable();
+            provinciesDT.Columns.Add("id", typeof(int));
+            provinciesDT.Columns.Add("naam", typeof(string));
+            //Gemeente
+            DataTable gemeenteDT = new DataTable();
+            gemeenteDT.Columns.Add("id", typeof(int));
+            gemeenteDT.Columns.Add("naam", typeof(string));
+            gemeenteDT.Columns.Add("provincie", typeof(int));
+            //Straat
+            DataTable straatDT = new DataTable();
+            straatDT.Columns.Add("id", typeof(int));
+            straatDT.Columns.Add("naam", typeof(string));
+            straatDT.Columns.Add("graaf", typeof(int));
+            straatDT.Columns.Add("gemeente", typeof(int));
+            //graaf
+            DataTable graafDT = new DataTable();
+            graafDT.Columns.Add("id", typeof(int));
+            //map
+            DataTable mapDT = new DataTable();
+            mapDT.Columns.Add("segment", typeof(int));
+            mapDT.Columns.Add("graaf", typeof(int));
+            //Vertices
+            DataTable verticesDT = new DataTable();
+            verticesDT.Columns.Add("verticeX",typeof(double));
+            verticesDT.Columns.Add("verticeY", typeof(double));
+            verticesDT.Columns.Add("segment", typeof(int));
+            //Segment
+            DataTable segmentDT = new DataTable();
+            segmentDT.Columns.Add("id", typeof(int));
+            segmentDT.Columns.Add("beginKnoop", typeof(int));
+            segmentDT.Columns.Add("eindKnoop", typeof(int));
+            //Knoop
+            DataTable knoopDT = new DataTable();
+            knoopDT.Columns.Add("id", typeof(int));
+            knoopDT.Columns.Add("knoopX", typeof(double));
+            knoopDT.Columns.Add("knoopY", typeof(double));
+            //punt
+            DataTable puntDT = new DataTable();
+            puntDT.Columns.Add("x", typeof(double));
+            puntDT.Columns.Add("y", typeof(double));
+            foreach (Provincie provincie in provincies)
+            {
+                provinciesDT.Rows.Add(provincie.provincieID, provincie.provincieNaam);
+            }
         }
     }
 }
