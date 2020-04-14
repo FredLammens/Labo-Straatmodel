2a65585efcc8bcee7e3507a16a83de3dcc1194e7
Frederic Lammens <frederic.lammens@student.hogent.be>
2020-04-14 17:57:40 +0200

    added quit to main menu

--- StraatModel2/Program.cs
+++ StraatModel2/Program.cs
@@ -16,10 +16,17 @@ namespace Labo
                 Console.WriteLine("| 1) Tool1 [rapport/Databestand]                                               |");
                 Console.WriteLine("| 2) Tool2 [SQLDatabase]                                                       |");
                 Console.WriteLine("| 3) Tool3 [SQLDatabase bevragingen]                                           |");
+                Console.WriteLine("| 4) Exit                                                                      |");
                 Console.WriteLine("--------------------------------------------------------------------------------");
-            int menuValue = Tool1.ValueChecker(3);
+            int menuValue = Tool1.ValueChecker(4);
             if (menuValue == 1)
-            Tool1.Start();
+                Tool1.Start();
+            else if (menuValue == 2)
+                Tool2.Start();
+            else if (menuValue == 3)
+                Tool3.Start();
+            else if (menuValue == 4)
+                return;
             //Console.WriteLine(Inlezer.Unzipper(@"C:\Users\Biebem\Downloads"));
         }
     }
--- StraatModel2/Tool3/Tool3.cs
+++ StraatModel2/Tool3/Tool3.cs
@@ -6,5 +6,36 @@ namespace StraatModel2.Tool3
 {
     class Tool3
     {
+        public static void Start()
+        {
+            Console.Clear();
+            string unzipPath = "";
+            int menuValue = 0;
+            while (menuValue != 4)
+            {
+                Console.WriteLine("--------------------------------Het wegennetwerk Tool 1-------------------------");
+                Console.WriteLine("| 1) Unzipper                                                                  |");
+                Console.WriteLine("| 2) Rapport                                                                   |");
+                Console.WriteLine("| 3) Databestand                                                               |");
+                Console.WriteLine("| 4) Exit                                                                      |");
+                Console.WriteLine("--------------------------------------------------------------------------------");
+                while (!int.TryParse(Console.ReadLine(), out menuValue))
+                    Console.WriteLine("Value is not valid , Please try again: ");
+                if (menuValue == 1)
+                    unzipPath = StartUnzipper();
+                else if (menuValue == 2)
+                    StartRapport(unzipPath);
+                else if (menuValue == 3)
+                    StartDatabestand(unzipPath);
+                else if (menuValue == 4)
+                {
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Value is not in menu range.");
+                }
+            }
+        }
     }
 }
