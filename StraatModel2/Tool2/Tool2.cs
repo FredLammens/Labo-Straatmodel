b8728ec1973f3db999659623693e7d8b9a32f134
Frederic Lammens <frederic.lammens@student.hogent.be>
2020-04-22 01:38:57 +0200

    added check for tool2

--- StraatModel2/Tool2/Tool2.cs
+++ StraatModel2/Tool2/Tool2.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace StraatModel2.Tool2
@@ -9,23 +10,29 @@ namespace StraatModel2.Tool2
         public static void Start()
         {
             Console.Clear();
-            Console.WriteLine("--------------------------------Het wegennetwerk Tool 2-------------------------");
-            Console.WriteLine("|                                   Deserialization                            |");
-            Console.WriteLine("| Gelieve het pad van de input file in te geven: (provincies.dat)              |");
-            Console.WriteLine("| exit om te stoppen                                                           |");
-            Console.WriteLine("--------------------------------------------------------------------------------");
-            string entered = Console.ReadLine();
-            if (entered.ToLower().Trim() == "exit")
+            bool ispath = false;
+            string entered = "";
+            while (!ispath)
             {
-                return;
+                Console.WriteLine("--------------------------------Het wegennetwerk Tool 2-------------------------");
+                Console.WriteLine("|                                   Deserialization                            |");
+                Console.WriteLine("| Gelieve het pad van de input file in te geven: (zonder provincies.dat)       |");
+                Console.WriteLine("| exit om te stoppen                                                           |");
+                Console.WriteLine("--------------------------------------------------------------------------------");
+                entered = Console.ReadLine();
+                if (entered.ToLower().Trim() == "exit")
+                {
+                    return;
+                }
+                if (Directory.Exists(entered)) 
+                {
+                    ispath = true;
+                }
             }
-            else
-            {
-                string pathInput = @"" + entered;
-                DatabaseImporter db = new DatabaseImporter(@"Data Source=DESKTOP-OF28PIK\SQLEXPRESS;Initial Catalog=provincies; Integrated Security=True");
-                db.InsertAll(Serializatie.DeSerializeProvinciesBinary(pathInput));
-                return;
-            }
+            string pathInput = @"" + entered;
+            DatabaseImporter db = new DatabaseImporter(@"Data Source=DESKTOP-OF28PIK\SQLEXPRESS;Initial Catalog=provincies; Integrated Security=True");
+            db.InsertAll(Serializatie.DeSerializeProvinciesBinary(pathInput));
+            return;
 
         }
     }
